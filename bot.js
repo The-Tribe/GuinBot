8a66cae3f3b9ce02b1aba32b0c69857d9a7b2272
Guinoala <37059120+Guinoala@users.noreply.github.com>
2018-03-05 22:56:29 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,19 @@
+ javascript
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I have awoken.');
+    console.log('Hello there.');
 });
 
 client.on('message', message => {
-    if (message.content === 'hi') {
-    	message.reply('Hi! Hello! Hey! Salutations! Greetings! Bonjour! Hola! Aloha! Guten Tag!');
-  	}
+    if (message.content === '!guin hi') {
+        message.reply('Hi!')
+    if (message.content === '!guin hello') {
+        message.reply('Hello!')
+    if (message.content === '!guin hey') {
+        message.reply('Hey!')
+      }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(NDIwMDc3OTYxMzcxMTg5MjQ5.DX5zsA.6fCitHPzoVn8FRmnatD4zs2AmIo)
