7992d393fe8b7104fa13bbd38d1557cd17ef2cd0
Guinoala <37059120+Guinoala@users.noreply.github.com>
2018-03-06 23:04:20 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on('ready', () => {client.user.setGame('!guin | Pokémon Go') })
     console.log('Hello there.');
 });
 
@@ -25,8 +25,17 @@ client.on('message', message => {
         message.reply('See you later alligator. :crocodile:')
     }
     if (message.content === '!guin who are you') {
-        message.reply('I am the bot of @Guinoala#6660')
+        message.reply('I am the bot of @Guinoala#6660.')
     }
-});
+    if (message.content === '!guin how rich are you') {
+        message.reply('$$ balance')
+    }
+    if (message.content === '!guin what is your discoin address') {
+        message.reply('$$ address')
+    }
+    if (message.content === '!guin give Guinoala some discoins') {
+        message.reply('$$ transfer discord:371054784398229516 1')
+    }
+  });
 
 client.login(process.argv[2])
