8b54b298e4d38d95d85d1170b547f3b5fd71b26a
Xx-Guinoala-xX <37059120+Guinoala@users.noreply.github.com>
2018-03-20 22:39:06 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,16 +17,12 @@ client.on("guildDelete", guild => {
 });
 
 client.on('message', async message => {
-    if(message.content.indexOf(config.prefix) !== 0) {
-	message.reply('I apologize. You have confused me.')
-	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-        const command = args.shift().toLowerCase();
+    if(message.content.indexOf(config.prefix) !== 0) return;
     }
     if(command === "ping") {
         const m = await message.channel.send("Ping?");
         m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
     }
-	
     if(command === "say") {
         const sayMessage = args.join(" ");
         message.delete().catch(O_o=>{}); 
