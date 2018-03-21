a23d5f49b07cce8bc1c0363873360b6787c23297
Xx-Guinoala-xX <37059120+Guinoala@users.noreply.github.com>
2018-03-20 19:06:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,46 +1,70 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./config.json");
 
 client.on('ready', () => {
 	client.user.setUsername('GuinBot');
-    console.log('Hello there.');
+    console.log('Hi! I baked you some fresh cupcakes.');
+
+client.on("guildCreate", guild => {
+	console.log(`I joined a new server: ${guild.name} (id: ${guild.id}). This server has ${guild.memberCount} members!`);
+  client.user.setGame(`!guin | ${client.guilds.size} servers`)
 });
 
-client.on('message', message => {
-    if (message.content === '!guin hi') {
-        message.reply('Well, hello there! :wave:')
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setGame(`!guin | ${client.guilds.size} servers`);
+	
+client.on('message', async message => {
+	if(message.content.indexOf(config.prefix) !== 0)
+		message.reply('I apologize. You have confused me.')
+	
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+	
+	if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+	if(command === "say") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }
+    if (command === "hi") {
+	message.reply('Well, hello there! :wave:')
     }
-    if (message.content === '!guin hello') {
+    if (command === 'hello') {
         message.reply('Hello! :wave:')
     }
-    if (message.content === '!guin hey') {
+    if (command === 'hey') {
         message.reply('Hi! :wave:')
     }
-    if (message.content === '!guin bye') {
+    if (command === 'bye') {
         message.reply('Farewell.')
     }
-    if (message.content === '!guin where do you live') {
+    if (command === 'where do you live') {
         message.reply('I live in an abandoned Burger King. :house_abandoned: :hamburger:')
     }
-    if (message.content === '!guin goodbye') {
+    if (command === 'goodbye') {
         message.reply('In a while crocodile. :crocodile:')
     }
-    if (message.content === '!guin who are you') {
+    if (command === 'who are you') {
         message.reply('I am GuinBot.')
     }
-    if (message.content === '!guin how rich are you') {
+    if (command === 'how rich are you') {
         message.reply('I do not own any currency. :money_with_wings:')
     }
-    if (message.content === '!guin who am I') {
+    if (command === 'who am I') {
         message.reply('You are one of my valued users. :hearts:')
     }
-    if (message.content === '!guin live') {
+    if (command === 'live') {
         message.reply('Back to life! Back to reality! :musical_note:')
     }
-    if (message.content === '!guin die') {
+    if (command === 'die') {
 	message.reply('I am physically incapable of death :skull:. I am a celestial, meaning that the power cosmic :rocket: keeps me alive for an infinite amount of time. :alarm_clock:')
     }
-    if (message.content === '!guin how old are you') {
+    if (command === 'how old are you') {
 	message.reply('I am fairly young, but cool young :cool:, not baby young. :baby:')
     }
 });
